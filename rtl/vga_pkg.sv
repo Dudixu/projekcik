f85f00350bd942996535eda4008c2d374a8f08c6
Dudixu <dmironiuk@wp.pl>
2024-07-17 18:40:48 +0200

    vga_pkg zmienione pod 1024x768
    CLK trzeba zmienić z 40MHz na 65Mhz

--- rtl/vga_pkg.sv
+++ rtl/vga_pkg.sv
@@ -9,21 +9,21 @@
 
 package vga_pkg;
 
-// Parameters for VGA Display 800 x 600 @ 60fps using a 40 MHz clock;
-localparam HOR_PIXELS = 800;
-localparam VER_PIXELS = 600;
-localparam HOR_TOTAL_TIME = 1056;
-localparam HOR_BLANK_START = 800;
-localparam HOR_SYNC_START = 840;
-localparam VER_TOTAL_TIME = 628;
-localparam VER_BLANK_START = 600;
-localparam VER_SYNC_START = 601;
+// Parameters for VGA Display 1024 x 768 @ 60fps using a 65 MHz clock;
+localparam HOR_PIXELS = 1024;
+localparam VER_PIXELS = 768;
+localparam HOR_TOTAL_TIME = 1344;
+localparam HOR_BLANK_START = 1024;
+localparam HOR_SYNC_START = 1048;
+localparam VER_TOTAL_TIME = 806;
+localparam VER_BLANK_START = 768;
+localparam VER_SYNC_START = 771;
 
 // Add VGA timing parameters here and refer to them in other modules.
-localparam HOR_SYNC_END = 968;
-localparam HOR_BLANK_END = 1056;
-localparam VER_SYNC_END = 605;
-localparam VER_BLANK_END = 628;
+localparam HOR_SYNC_END = 1184;
+localparam HOR_BLANK_END = 1344;
+localparam VER_SYNC_END = 777;
+localparam VER_BLANK_END = 806;
 
 localparam RECT_LENGTH = 48;
 localparam RECT_HEIGHT = 64;
