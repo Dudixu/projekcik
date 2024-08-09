7e98a24f900d2e929291cb202277af5dace178fd
Dudixu <116211727+Dudixu@users.noreply.github.com>
2024-08-10 00:21:33 +0200

    Update mouse_position.sv

--- rtl/Rect/mouse_position.sv
+++ rtl/Rect/mouse_position.sv
@@ -16,6 +16,7 @@ module mouse_position
     input logic        clk,
     input logic        rst,
     input logic        LMB,
+    input logic [3:0] board [0:7][0:7],
     input logic [11:0] mouse_xpos,
     input logic [11:0] mouse_ypos,
     output logic [5:0] mouse_position, // miejsce na planszy na którym znajduje sie myszka
@@ -25,14 +26,11 @@ module mouse_position
 
 import vga_pkg::*; 
 
-logic LMB_pressed;
-
-always_ff @(posedge clk) begin : xypos_blk
+    always_ff @(posedge clk, posedge rst, posedge LMB) begin : xypos_blk
     if(rst) begin
         mouse_position <= '0;
         pick_piece <= '0;
         place_piece <= '0;
-        LMB_pressed <= '0;
     end else begin
         if(LMB == 1 & LMB_pressed == 0) begin
             LMB_pressed <= 1;
